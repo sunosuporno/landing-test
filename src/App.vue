1f9245acd4342716b4231e42e3ddc6ec3305d743
sunosuporno <sarkarsuporno36@gmail.com>
2022-07-27 21:12:16 +0530

    optimised for lg and 2xl screens

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="bg-[#111111] text-white">
-    <div class="max-w-screen min-h-screen">
-      <div class="w-11/12 mx-auto pt-3">
+    <div class="max-w-[1734px] min-h-screen pb-5 lg:pb-10">
+      <div class="w-11/12 mx-auto pt-3 2xl:pt-5">
         <div class="flex justify-between">
           <div class="flex flex-row items-center">
             <img
               src="./assets/repofi-r.svg"
-              class="h-12 md:h-20 xl:h-14 mr-2"
+              class="h-12 md:h-20 lg:h-16 xl:h-14 2xl:h-20 mr-2"
               alt="repofi-logo"
             />
-            <p class="font-logo text-2xl md:text-3xl xl:text-2xl -mb-[6px]">
+            <p
+              class="font-logo text-2xl md:text-3xl lg:text-2xl xl:text-2xl 2xl:text-4xl -mb-[6px]"
+            >
               RepoFi
             </p>
           </div>
@@ -18,7 +20,7 @@
               <a href="https://twitter.com/repofime" target="_blank">
                 <img
                   src="./assets/twitter-white.svg"
-                  class="w-10 md:w-12 xl:w-10 px-[2px] py-[7px] 2xl:w-14 rounded-full"
+                  class="w-10 md:w-12 lg:w-10 xl:w-10 px-[2px] py-[7px] 2xl:w-14 rounded-full"
                   alt="Follow our Twitter!"
                 />
               </a>
@@ -27,7 +29,7 @@
               <a href="https://discord.gg/sJu3cyWpzz" target="_blank">
                 <img
                   src="./assets/discord-white.svg"
-                  class="w-12 md:w-14 xl:w-12 px-1 py-2 2xl:w-16"
+                  class="w-12 md:w-14 lg:w-12 xl:w-12 px-1 py-2 2xl:w-16"
                   alt="Join the community in our Discord"
                 />
               </a>
@@ -49,39 +51,45 @@
       <img src="./assets/Union.svg" class="" alt="" /> -->
         <img
           src="./assets/asset.gif"
-          class="w-[600px] h-auto lg:w-2/3 lg:h-2/3 xl:w-1/3 xl:h-1/3 mx-auto hero-image -z-10"
+          class="w-[600px] h-auto lg:h-[32em] lg:w-auto xl:w-1/3 xl:h-1/3 2xl:w-2/5 2xl:h-auto mx-auto hero-image -z-10"
           alt="Image"
         />
         <div
-          class="absolute ml-6 md:ml-10 lg:left-[16em] xl:left-[3em] lg:right-0 xl:right-0 top-[16em] md:top-80 xl:top-[16em] w-11/12 lg:w-4/6 xl:w-4/5"
+          class="absolute ml-6 md:ml-10 lg:left-[2em] xl:left-[3em] lg:right-0 xl:right-0 top-[16em] md:top-80 lg:top-72 xl:top-[16em] 2xl:top-[24em] w-11/12 lg:w-4/6 xl:w-4/5"
         >
           <p
             class="font-heading font-normal uppercase text-6xl md:text-7xl xl:text-8xl"
           >
             It pays to be you
           </p>
-          <p class="mt-6 text-xl md:text-3xl w-3/5 font-body">
+          <p
+            class="mt-6 text-xl md:text-3xl lg:text-2xl 2xl:3xl w-3/5 font-body"
+          >
             All your links and NFTs at one place. Make connections. Make
             payments.
           </p>
         </div>
       </div>
-      <div class="mt-14 flex flex-col items-center">
-        <p class="text-xl mb-5 font-body">Sign up for the early beta!</p>
+      <div class="mt-14 lg:mt-10 flex flex-col items-center">
+        <p class="text-xl 2xl:text-3xl mb-5 font-body">
+          Sign up for the early beta!
+        </p>
         <form
-          class="rounded-xl xl:w-5/12 xl:h-14 bg-gradient-to-b from-[#ededed] to-[#000000] p-1"
+          class="rounded-xl xl:w-5/12 xl:h-14 2xl:h-16 bg-gradient-to-b from-[#ededed] to-[#000000] p-[2px] 2xl:p-[3px]"
         >
           <div class="h-full w-full rounded-xl bg-[#111111]">
-            <div class="w-full h-full flex flex-row items-center">
+            <div
+              class="w-full h-full flex flex-row items-center justify-evenly"
+            >
               <input
                 type="email"
                 placeholder="gm@gtfol.com"
-                class="xl:w-11/12 h-full bg-[#111111] px-2 py-1 rounded-xl hover:outline-none focus:outline-none font-body"
+                class="xl:w-11/12 2xl:text-2xl h-full bg-[#111111] px-2 py-1 rounded-xl hover:outline-none focus:outline-none font-body"
               />
               <button class="text-gray" @click="handleClick">
                 <img
                   src="./assets/arrow-right.svg"
-                  class="w-7 xl:w-10"
+                  class="w-7 xl:w-10 2xl:w-12 pr-1"
                   alt="submit your email"
                 />
               </button>
